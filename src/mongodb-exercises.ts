8758a0b9e011e83e6e271804847dc07aed8c6089
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-09-22 13:20:08 +0200

    second/third commit

--- src/mongodb-exercises.ts
+++ src/mongodb-exercises.ts
@@ -25,8 +25,16 @@ async function connect() {
 
     const db: Db = await client.db('Exercise')
     const col: Collection<Animal> = db.collection<Animal>('animalFacts')
-
-    await fidingTheElephant(col)
+    const rabbit: Animal = {
+      species: 'Rabbit',
+      factoid: 'Rabbits are extremely cute',
+      score: 25,
+    }
+    await fidingThePingvin(col)
+    await findingHighScoreAnimals(col)
+    // await insertRabbit(col, rabbit)
+    await deleteRabbit(col)
+    // await updateRabbit(col)
 
     await client.close()
   } catch (error: any) {
@@ -34,14 +42,71 @@ async function connect() {
   }
   console.log('Program executed successfully.')
 }
-async function fidingTheElephant(col: Collection<Animal>): Promise<void> {
+async function fidingThePingvin(col: Collection<Animal>): Promise<void> {
   const filter = { score: 32 }
   const found: WithId<Animal> | null = await col.findOne(filter)
   if (found) {
-    console.log('The animal you were serching for is: ' + found)
+    console.log('The animal you were serching for is: ', found)
   } else {
     console.log('The animal is out somewhere')
   }
 }
+async function findingHighScoreAnimals(col: Collection<Animal>): Promise<void> {
+  const filter = { score: { $gte: 60 } }
+  const cursor: FindCursor<WithId<Animal>> = col.find(filter)
+  const found: WithId<Animal>[] = await cursor.toArray()
 
+  if (found.length < 1) {
+    console.log('No animal is cool enough:(')
+    return
+  }
+  found.forEach((animal) => {
+    console.log(
+      `${animal.species} has a score of ${animal.score} and intresting fact: ${animal.factoid}`
+    )
+  })
+}
+
+async function insertRabbit(
+  col: Collection<Animal>,
+  rabbit: Animal
+): Promise<ObjectId | null> {
+  const result: InsertOneResult<Animal> = await col.insertOne(rabbit)
+  if (!result.acknowledged) {
+    console.log('Could not insert rabbit')
+    return null
+  }
+  console.log(`${rabbit.species} was added to your collection`)
+
+  return result.insertedId
+}
+
+async function deleteRabbit(col: Collection<Animal>): Promise<void> {
+  const filter = { species: 'Easter Bunny' }
+  const result: DeleteResult = await col.deleteMany(filter) //deleteOne tar bort en, deleteMany alla
+  if (!result.acknowledged) {
+    console.log('Could not delete any rabbits')
+    return
+  }
+
+  console.log(`Deleted ${result.deletedCount} rabbit`)
+}
+
+async function updateRabbit(col: Collection<Animal>): Promise<void> {
+  const filter = { species: 'Rabbit' }
+  const updateFilter = {
+    $set: {
+      species: 'Easter Bunny',
+      score: 58,
+    },
+  }
+  const result: UpdateResult<Animal> = await col.updateOne(filter, updateFilter)
+  if (!result.acknowledged) {
+    console.log('Could not update any rabbits')
+    return
+  }
+  console.log(
+    `Matched ${result.matchedCount} documents and modified ${result.modifiedCount}`
+  )
+}
 connect()
